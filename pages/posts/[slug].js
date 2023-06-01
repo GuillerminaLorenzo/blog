ae2c4a31fba3d530597e9ef0a1f366f4e37255b8
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-01 17:39:53 +0100

    Updated PostDetail page with PostContent component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,3 +1,5 @@
+import PostContent from "../../components/posts/post-detail/post-content";
+
 export default function PostDetailPage() {
-  return <h1>PostDetailPage</h1>;
+  return <PostContent />;
 }

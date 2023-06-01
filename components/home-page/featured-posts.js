080967cf61160cbd40ff8ae5c6383d9a94cee7a1
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-01 14:41:08 +0100

    Updated FeaturedPosts component with PostGrid component

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,9 +1,11 @@
+import PostsGrid from "../posts/posts-grid";
 import FeaturedPostsStyle from "./featured-posts.module.css";
 
-export default function FeaturedPosts() {
+export default function FeaturedPosts(props) {
   return (
     <section className={FeaturedPostsStyle.latest}>
       <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
     </section>
   );
 }

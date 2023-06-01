029d418b0b363759e189fe52c3bb5595e5cbd53a
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-01 11:29:16 +0100

    Updated featured posts

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,9 @@
+import FeaturedPostsStyle from "./featured-posts.module.css";
+
+export default function FeaturedPosts() {
+  return (
+    <section className={FeaturedPostsStyle.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}

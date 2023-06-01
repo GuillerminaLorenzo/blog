11de6160eec187be55d89a8cf5f7253cfa623cb7
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-01 14:46:58 +0100

    Updated PostItem component, formatted linkPath and Image layout

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -13,12 +13,19 @@ export default function PostItem(props) {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={PostItemStyle.post}>
-      <Link>
+      <Link href={linkPath}>
         <div className={PostItemStyle.image}>
-          <Image scr={imagePath} alt={title} width={300} height={200} />
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={PostItemStyle.content}>
           <h3>{title}</h3>

c4258e756eb69edbce0edb9690af34ea7a098b07
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-05 17:33:24 +0100

    Updated handler, added connection to database to store messages, added try/catch

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -10,7 +12,7 @@ export default function handler(req, res) {
       !message ||
       message.trim() === ""
     ) {
-      res.status(422).json({ message: "Invalid input" });
+      res.status(422).json({ message: "Invalid input." });
       return;
     }
 
@@ -20,7 +22,29 @@ export default function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://guille:nhWGAJy3MnLwUMEe@cluster0.8trbvr1.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db("my-site");
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)

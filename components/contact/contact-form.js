e0ab236c65f7037a6dbe2e267476876e431c95bb
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-05 14:58:05 +0100

    Updated ContactForm page , added sendMessageHandler

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,63 @@
+import { useState } from "react";
+
 import ContactFormStyle from "./contact-form.module.css";
 
 export default function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section className={ContactFormStyle.contact}>
       <h1>How can I help you?</h1>
-      <form className={ContactFormStyle.form}>
+      <form className={ContactFormStyle.form} onSubmit={sendMessageHandler}>
         <div className={ContactFormStyle.controls}>
           <div className={ContactFormStyle.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
           <div className={ContactFormStyle.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
         <div className={ContactFormStyle.control}>
           <label htmlFor="message">Your Email</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
         </div>
 
         <div className={ContactFormStyle.actions}>
